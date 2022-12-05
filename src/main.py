f0bbfce2cfdecfabd7d4a14798ffe104b6c05ccf
Alexandre Lemonnier <alexandre.lemonnier@epita.fr>
2022-12-05 17:07:55 +0100

    begin to copy crowddynamics

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import sys
+from PySide6 import QtWidgets
+from main_win import MainWindow
+
+def run():
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+    
+if __name__ == "__main__":
+    run()
