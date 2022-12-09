042a69bff732ef21a6fc56754f98253756cf6fb0
Sarah Gutierez <sarah.gutierez@epita.fr>
2022-12-09 18:19:27 +0100

    refacto(map): change lines to polygon for shibuya

--- src/qtgui/acu/shibuya/fields.py
+++ src/qtgui/acu/shibuya/fields.py
@@ -8,48 +8,40 @@ from crowddynamics.simulation.field import Field
 
 
 def rectangle(x, y, width, height):
-    return Polygon([(x, y), (x + width, y), (x + width, y + height), (x, y + height)])
-
+    return Polygon(
+        [(x, y), (x + width, y), (x + width, y + height), (x, y + height)])
 
 class ShibuyaField(Field):
-    width = 30
-    height = 20
-    out_size = 2
-    left = 4
+    width = 1369.0
+    height = 982.0
+    ratio = Float(
+        default_value=1 / 3,
+        min=0, max=1)
 
-    @default("obstacles")
+    @default('obstacles')
     def _default_obstacles(self):
-        return (
-            LineString([(0, self.height), (self.left, self.height)])
-            | LineString([(self.left + self.out_size, self.height), (self.width, self.height)])
-            | LineString([(0, self.height - self.left), (self.left, self.height - self.left)])
-            | LineString(
-                [
-                    (self.left + self.left, self.height - self.left),
-                    (self.width, self.height - self.left),
-                ]
-            )
-            | LineString([(self.left, 0), (self.left, self.height - self.left)])
-            | LineString([(self.left + self.left, 0), (self.left + self.left, self.height - self.left)])
-        )
+        return Polygon([(400,553), (416, 370), (437, 333), (610, 324), (615, 338)]) | \
+            Polygon([(434, 238), (584, 238), (574, 0), (434, 0) ]) | \
+            Polygon([(323, 390), (318, 550), (0, 520), (0, 0), (120, 0), ]) | \
+            Polygon([(832, 312), (798, 363), (814, 404), (815, 580), (762, 626), (465, 634), (462, 628), (705, 386), (744, 378), (802, 286), (1212, 0), (1260, 0) ]) | \
+            Polygon([(764, 982), (746, 688), (474, 700), (510, 982)])|\
+            Polygon([(1369, 492), (1136, 438), (878, 414), (877, 580), (1136, 600), (1369, 662) ])
 
-    @default("targets")
+    @default('targets')
     def _default_targets(self):
-        return [LineString([(self.left, self.height), (self.left + self.out_size, self.height)])]
+        return [LineString([(0, 520), (510, 982)]),
+                # LineString([(764, 982), (1025, 591)]),
+                # LineString([(878, 414), (814, 404)]),
+                # LineString([(744, 378), (584, 238)]),
+                LineString([(434, 238), (323, 390)]),
+            ]
 
-    @default("spawns")
+    @default('spawns')
     def _default_spawns(self):
-        return [
-            rectangle(0, self.height - self.left, self.left, self.left),
-            rectangle(self.left, 0, self.left, self.height - self.left),
-            rectangle(
-                self.left + self.out_size,
-                self.height - self.left,
-                self.width - self.out_size - self.left,
-                self.left,
-            ),
+        return [Polygon([(318, 550), (400, 553), (462, 628), (474, 700)]),
+                Polygon([(584, 238), (610, 324), (615, 338), (705, 386)])
         ]
 
-    @default("domain")
+    @default('domain')
     def _default_domain(self):
-        return rectangle(0, 0, self.width, self.height)
+        return self.convex_hull()
