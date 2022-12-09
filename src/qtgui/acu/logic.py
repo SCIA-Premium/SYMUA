3d2ee629befa5e91cbe0624a9a4f319178e48d57
Baptiste Bourdet <baptiste.bourdet@epita.fr>
2022-12-09 15:50:11 +0100

    feat: optimize panic with numpy

--- src/qtgui/acu/logic.py
+++ src/qtgui/acu/logic.py
@@ -1,6 +1,6 @@
 import numpy as np
 from traitlets.traitlets import Float, Int
-from cell_lists import add_to_cells, neighboring_cells 
+from cell_lists import add_to_cells, neighboring_cells
 from crowddynamics.core.geometry import geom_to_linear_obstacles
 from crowddynamics.core.steering.collective_motion import more_than_five_neighbors, find_nearest_neighbors
 from crowddynamics.core.sensory_region import is_obstacle_between_points
@@ -15,9 +15,11 @@ class DeleteDeadAgent(LogicNode):
         agents = self.simulation.agents.array
         self.simulation.agents.array = np.delete(agents, np.where(~agents["active"]), axis=0)
         self.simulation.agents.array = np.delete(agents, np.where(agents["oxygen"] <= 0), axis=0)
-        
+
+
 class PanicAgent(LogicNode):
     """Logic to make agents panic if too many people are around"""
+
     sight_neighbors = Float(
         default_value=10.0,
         min=0,
@@ -29,31 +31,46 @@ class PanicAgent(LogicNode):
         help="Maximum number of nearest agents inside sight_herding radius " "that herding agent are following.",
     )
 
+    spread_factor = Float(default_value=0.05, min=0, max=1, help="Factor of spread to neighbors when panic")
+
     def update(self):
         agents = self.simulation.agents.array
-        for i, agent in enumerate(agents):
-            if agent["is_panic"] and agent["panic"] < START_PANIC:
-                agent["is_panic"] = False
-            elif agent["is_panic"]:
-                agent["target_direction"] = -agent["target_direction"]
-            elif agent["panic"] > START_PANIC:
-                agent["is_panic"] = True
-                agent["target_direction"] = -agent["target_direction"]
-                
-                field = self.simulation.field
-                obstacles = geom_to_linear_obstacles(field.obstacles)
-                position = agents["position"]
-                sight = self.sight_neighbors
-                
-                points_indices, cells_count, cells_offset, grid_shape = add_to_cells(agents["position"], sight)
-                cell_indices = np.arange(len(cells_count))
-                neigh_cells = neighboring_cells(grid_shape)
-                neighbors = find_nearest_neighbors(
-                    position, sight,
-                    self.size_nearest_other, cell_indices, neigh_cells, points_indices,
-                    cells_count, cells_offset, obstacles)
-                for j in neighbors[i]:
-                    agents[j]['panic'] += START_PANIC 
+
+        no_panic = np.where((agents["is_panic"] == True) & (agents["panic"] < START_PANIC))
+        agents["is_panic"][no_panic] = False
+        agents["target_direction"][agents["is_panic"] == True] = -agents["target_direction"][agents["is_panic"] == True]
+        new_panic = np.where((~agents["is_panic"]) & (agents["panic"] > START_PANIC))
+        agents["is_panic"][new_panic] = True
+        agents["target_direction"][new_panic] = True
+
+        delta_panic = np.zeros(agents.size, dtype=np.int64)
+
+        field = self.simulation.field
+        obstacles = geom_to_linear_obstacles(field.obstacles)
+        position = agents["position"]
+        sight = self.sight_neighbors
+
+        points_indices, cells_count, cells_offset, grid_shape = add_to_cells(agents["position"], sight)
+        cell_indices = np.arange(len(cells_count))
+        neigh_cells = neighboring_cells(grid_shape)
+        neighbors = find_nearest_neighbors(
+            position,
+            sight,
+            self.size_nearest_other,
+            cell_indices,
+            neigh_cells,
+            points_indices,
+            cells_count,
+            cells_offset,
+            obstacles,
+        )
+
+        for i in np.where(agents[agents["is_panic"]]):
+            for j in neighbors[i]:
+                delta_panic[j] = np.ceil(self.spread_factor * START_PANIC)
+
+        agents["panic"] += delta_panic
+
 
 class TooManyPeople(LogicNode):
     """Logic to kill people that are to pack"""
@@ -78,7 +95,7 @@ class TooManyPeople(LogicNode):
         points_indices, cells_count, cells_offset, grid_shape = add_to_cells(agents["position"], sight)
         cell_indices = np.arange(len(cells_count))
         neigh_cells = neighboring_cells(grid_shape)
-        math = (
+        nb_neighbours = (
             more_than_five_neighbors(
                 position,
                 sight,
@@ -92,11 +109,11 @@ class TooManyPeople(LogicNode):
             )
             - 5
         )
-        t1 = agents["panic"] + math**3
-        t = agents["oxygen"] - math**3
-        t1 = np.where(t1 < BASE_PANIC, BASE_PANIC, t1)
-        agents["panic"] = t1
-        agents["oxygen"] = np.where(t > BASE_OXYGEN, BASE_OXYGEN, t)
+        new_panic = agents["panic"] + nb_neighbours**3
+        new_oxygen = agents["oxygen"] - nb_neighbours**3
+        new_panic = np.where(new_panic < BASE_PANIC, BASE_PANIC, new_panic)
+        agents["panic"] = new_panic
+        agents["oxygen"] = np.where(new_oxygen > BASE_OXYGEN, BASE_OXYGEN, new_oxygen)
 
 
 class Killclose(LogicNode):
@@ -104,4 +121,3 @@ class Killclose(LogicNode):
 
     def update(self):
         self.simulation.agents.array["active"] = 0
-        
